a599ee4b7a1f8953fd1182b7ae09b69ee0762b79
bernie-skipole <bernie@skipole.co.uk>
2021-08-30 17:45:45 +0100

    working on motor

--- doordriver.py
+++ doordriver.py
@@ -451,28 +451,42 @@ class _DOOR:
         "Request an update from the pico"
         self.rconn.publish('tx_to_pico', 'pico_roof')
 
+
     @property
-    def status(self):
-        """Monitors the door, and returns the door status, one of OPEN CLOSED OPENING CLOSING"""
+    def status_code0(self, door):
+        """Monitors door ( equal to 0 or 1) and returns status code"""
         # status is a numeric code
+        # 0 : unknown
         # 1 : open
         # 2 : opening
         # 3 : closed
         # 4 : closing
-
         # could also receive
         # 5 : Error - believed open, but limit switch has not closed
         # 6 : Error - believed closed, but limit switch has not closed
+        roof_status = self.rconn.get(f'pico_roofdoor{door}')
+        if roof_status is None:
+            return 0
+        return int(roof_status)
 
-        roof_status1 = self.rconn.get('pico_roofdoor1')
-        roof_status0 = self.rconn.get('pico_roofdoor0')
-        if roof_status0 is None:
-            return
-        if roof_status1 is None:
-            return
 
-        status0 = int(roof_status0)
-        status1 = int(roof_status1)
+    @property
+    def status(self):
+        """Monitors the door, and returns the door status, one of OPEN CLOSED OPENING CLOSING
+           returns None, if status unknown.
+           On Error, always return CLOSED, to prevent telescope operation"""
+
+        status1 = self.status_code0(1)
+        status0 = self.status_code0(0)
+
+        if not status0:
+            self.alarm = True
+            self.alarmtext = "Error - Left Door alert, status unknown."
+            status0 = 3
+        if not status1:
+            self.alarm = True
+            self.alarmtext = "Error - Right Door alert, status unknown."
+            status1 = 3
 
         if (status0 > 4) or (status1 > 4):
             # set commen alarm signal
@@ -481,16 +495,16 @@ class _DOOR:
             self.alarm = False
 
         if status0 == 5:
-            self.alarmtext = "Error - Left Door believed open, but limit switch has not closed"
-            status0 = 1
+            self.alarmtext = "Error - Left Door alert, OPEN limit switch has not closed"
+            status0 = 3
         if status0 == 6:
-            self.alarmtext = "Error - Left Door believed closed, but limit switch has not closed"
+            self.alarmtext = "Error - Left Door alert, CLOSED limit switch has not closed"
             status0 = 3
         if status1 == 5:
-            self.alarmtext = "Error - Right Door believed open, but limit switch has not closed"
-            status1 = 1
+            self.alarmtext = "Error - Right Door alert, OPEN limit switch has not closed"
+            status1 = 3
         if status1 == 6:
-            self.alarmtext = "Error - Right Door believed closed, but limit switch has not closed"
+            self.alarmtext = "Error - Right Door alert, CLOSED limit switch has not closed"
             status1 = 3
 
         # both doors must be the same to set the status
