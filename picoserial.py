a644c72fa57c655c2b8337e8eb7caf089525716a
bernie-skipole <bernie@skipole.co.uk>
2021-10-04 22:01:03 +0100

    making door parameters controllable from pi

--- picoserial.py
+++ picoserial.py
@@ -63,8 +63,42 @@ def receiver(ser, rconn):
         else:
             # it is door motor 0
             rconn.set('pico_roofdoor0', int.from_bytes([returnval[2]], 'big')  )
+    elif returnval[0] == 12:
+        # parameter values for door0
+        if (returnval[1] == 1):
+            # fast duration
+            rconn.set('pico_door0_fast_duration', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 2):
+            # duration
+            rconn.set('pico_door0_duration', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 3):
+            # max_running_time
+            rconn.set('pico_door0_max_running_time', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 4):
+            # maximum ratio
+            rconn.set('pico_door0_maximum', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 5):
+            # minimum ratio ratio
+            rconn.set('pico_door0_minimum', int.from_bytes([returnval[2]], 'big')  )
+    elif returnval[0] == 13:
+        # parameter values for door1
+        if (returnval[1] == 1):
+            # fast duration
+            rconn.set('pico_door1_fast_duration', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 2):
+            # duration
+            rconn.set('pico_door1_duration', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 3):
+            # max_running_time
+            rconn.set('pico_door1_max_running_time', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 4):
+            # maximum ratio
+            rconn.set('pico_door1_maximum', int.from_bytes([returnval[2]], 'big')  )
+        if (returnval[1] == 5):
+            # minimum ratio ratio
+            rconn.set('pico_door1_minimum', int.from_bytes([returnval[2]], 'big')  )
 
-       
+   
 
 
 def sender(data, ser, rconn):
@@ -72,53 +106,118 @@ def sender(data, ser, rconn):
     if data == b'pico_led_On':
         # turns on led
         bincode = bytes([1, 25, 1, 255])  # send bytes to pico
-        ser.write(bincode)
         rconn.set('pico_led', 'On')  # save value in redis
     elif data == b'pico_led_Off':
         # turns off led
         bincode = bytes([1, 25, 0, 255])  # send bytes to pico
-        ser.write(bincode)
         rconn.set('pico_led', 'Off')
     elif data == b'pico_led':
         # requests led status
         bincode = bytes([2, 25, 0, 255])
-        ser.write(bincode)
     elif data.startswith(b'pico_monitor_'):
         # monitor data is of the form pico_monitor_0, pico_monitor_1 etc..
         count = int(data[13:])
         # count is the number passed in the data bytes string
         bincode = bytes([3, 0, count, 255])  # send monitor request to pico
-        ser.write(bincode)
     elif data == b'pico_temperature':
         bincode = bytes([5, 4, 0, 255])  # send temperature request to pico
-        ser.write(bincode)
     elif data == b'pico_roof':
         # ask for roof status
         bincode = bytes([6, 1, 0, 255])  # send request for status for door number 0
         ser.write(bincode)
         bincode = bytes([6, 1, 1, 255])  # send request for status for door number 1
-        ser.write(bincode)
     # the following opens/closes both doors
     elif data == b'pico_roof_open':
         bincode = bytes([9, 0, 0, 255])  # send request to open both doors to pico
-        ser.write(bincode)
     elif data == b'pico_roof_close':
         bincode = bytes([9, 0, 1, 255])  # send request to close both doors to pico
-        ser.write(bincode)
     # the following allows control of individual roof doors - not used in normal operation
     elif data == b'pico_roof0_open':
         bincode = bytes([8, 0, 0, 255])  # send request to open door 0 to pico
-        ser.write(bincode)
     elif data == b'pico_roof0_close':
         bincode = bytes([8, 0, 1, 255])  # send request to close door 0 to pico
-        ser.write(bincode)
     elif data == b'pico_roof1_open':
         bincode = bytes([8, 1, 0, 255])  # send request to open door 1 to pico
-        ser.write(bincode)
     elif data == b'pico_roof1_close':
         bincode = bytes([8, 1, 1, 255])  # send request to close door1 to pico
-        ser.write(bincode)
 
+    elif data.startswith(b'pico_set_door0_fast_duration_'):
+        # value is the byte after the above string
+        value = int(data[29:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([10, 1, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door0_duration_'):
+        # value is the byte after the above string
+        value = int(data[24:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([10, 2, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door0_max_running_time_'):
+        # value is the byte after the above string
+        value = int(data[32:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([10, 3, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door0_maximum_'):
+        # value is the byte after the above string
+        value = int(data[23:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([10, 4, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door0_minimum_'):
+        # value is the byte after the above string
+        value = int(data[23:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([10, 5, value, 255])  # sends request to pico
+ 
+    elif data.startswith(b'pico_set_door1_fast_duration_'):
+        # value is the byte after the above string
+        value = int(data[29:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([11, 1, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door1_duration_'):
+        # value is the byte after the above string
+        value = int(data[24:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([11, 2, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door1_max_running_time_'):
+        # value is the byte after the above string
+        value = int(data[32:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([11, 3, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door1_maximum_'):
+        # value is the byte after the above string
+        value = int(data[23:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([11, 4, value, 255])  # sends request to pico
+    elif data.startswith(b'pico_set_door1_minimum_'):
+        # value is the byte after the above string
+        value = int(data[23:])
+        # value is the number passed in the data bytes string
+        bincode = bytes([11, 5, value, 255])  # sends request to pico
+
+    elif data.startswith(b'pico_get_door0_fast_duration'):
+        bincode = bytes([12, 1, 0, 255])
+    elif data.startswith(b'pico_get_door0_duration'):
+        bincode = bytes([12, 2, 0, 255])
+    elif data.startswith(b'pico_get_door0_max_running_time'):
+        bincode = bytes([12, 3, 0, 255])
+    elif data.startswith(b'pico_get_door0_maximum'):
+        bincode = bytes([12, 4, 0, 255])
+    elif data.startswith(b'pico_get_door0_minimum'):
+        bincode = bytes([12, 5, 0, 255])
+
+    elif data.startswith(b'pico_get_door1_fast_duration'):
+        bincode = bytes([13, 1, 0, 255])
+    elif data.startswith(b'pico_get_door1_duration'):
+        bincode = bytes([13, 2, 0, 255])
+    elif data.startswith(b'pico_get_door1_max_running_time'):
+        bincode = bytes([13, 3, 0, 255])
+    elif data.startswith(b'pico_get_door1_maximum'):
+        bincode = bytes([13, 4, 0, 255])
+    elif data.startswith(b'pico_get_door1_minimum'):
+        bincode = bytes([13, 5, 0, 255])
+
+    else:
+        return
+    ser.write(bincode)
 
 
 
